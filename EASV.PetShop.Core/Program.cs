fad3c4761e732d1aef3a705fab010b517683e5a3
Mathias Kristensen <mathiaskristensen52@gmail.com>
2021-08-25 09:39:54 +0200

    Initial commit

---
+++ EASV.PetShop.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EASV.PetShop.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
