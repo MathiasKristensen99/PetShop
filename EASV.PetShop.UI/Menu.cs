4c8c61524324c925b90f8386a0d539ee13900e49
Mathias Kristensen <mathiaskristensen52@gmail.com>
2021-08-25 11:05:27 +0200

    Yeah bro

--- EASV.PetShop.UI/Menu.cs
+++ EASV.PetShop.UI/Menu.cs
@@ -1,4 +1,5 @@
-﻿using EASV.PetShop.Core.IServices;
+﻿using System;
+using EASV.PetShop.Core.IServices;
 
 namespace EASV.PetShop.UI
 {
@@ -6,9 +7,37 @@ namespace EASV.PetShop.UI
     {
         private IPetService _petService;
 
-        public Menu(IPetService service)
+        public Menu(IPetService petService)
         {
-            _petService = service;
+            _petService = petService;
+        }
+
+        public void Start()
+        {
+            StartLoop();
+        }
+
+        private void StartLoop()
+        {
+            int choice;
+            while ((choice = GetMainMenuSelection()) != 0)
+            {
+                if (choice == 1)
+                {
+                    
+                }
+            }
+        }
+        
+        private int GetMainMenuSelection()
+        {
+            var selectionString = Console.ReadLine();
+            int selection;
+            if (int.TryParse(selectionString, out selection))
+            {
+                return selection;
+            }
+            return -1;
         }
     }
 }
--- EASV.PetShop.UI/Program.cs
+++ EASV.PetShop.UI/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using EASV.PetShop.Domain.Models;
 
 namespace EASV.PetShop.UI
 {
